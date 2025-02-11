8b71353a48a760ac3ddc1694accdbd113e7f1cc0
sitati-elsis <sitatielsis@gmail.com>
2025-02-11 13:48:32 +0300

    Add user session

--- app.js
+++ app.js
@@ -4,12 +4,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const Session = require('express-session');
+const MongoDBSessionStore = require('connect-mongodb-session')(Session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 // const User = require('./models/user');
 
+dotenv.config();
+
 const app = express();
+const sessionStore = new MongoDBSessionStore({
+  uri: process.env.MONGODB_URL,
+  collection: 'sessions',
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -18,10 +26,16 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
-dotenv.config();
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  Session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: sessionStore,
+  })
+);
 
 app.use((req, res, next) => {
   User.findById('67a5f3d31e80cf396d9024d9')
